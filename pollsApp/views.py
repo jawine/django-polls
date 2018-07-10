6ed3e69b6ba249243435427736c8a890bde55302
Joel Wineland <jawineland@outlook.com>
2018-07-10 01:34:36 -0500

    Completed part 3

--- pollsApp/views.py
+++ pollsApp/views.py
@@ -1,7 +1,39 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+
+from .models import Question
+
 
 def index(request):
-    return HttpResponse("Hola mundo")
+    # retrieve list of questions from Question model
+    # retrieve up to 5
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, 'pollsApp/index.html', context)
+
+def detail(request, question_id):
+    """Can use try catch block
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 
+        'pollsApp/detail.html',
+        {'question': question})
+    """
+    # or use get_object_or_404 shortcut
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 
+        'pollsApp/detail.html',
+        {'question': question})
 
 
+def results(request, question_id):
+    response = "Viewing results of question %s." 
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("Voting on question %s." %
+        question_id)
